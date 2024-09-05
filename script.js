4ca8f235aaf2b5a0960ea5f6d1cd4dc9fe48311e
ginapin <101413119+ginapin@users.noreply.github.com>
2024-09-06 05:02:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-document.getElementById('submit').addEventListener('click', function() {
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
-    const paymentProof = document.getElementById('paymentProof').files[0];
+// Load the API client and auth2 library
+gapi.load('client:auth2', initClient);
 
-    if (date && time && paymentProof) {
-        alert(`Booking confirmed for ${date} at ${time}. Payment proof uploaded.`);
-    } else {
-        alert('Please select a date, time, and upload payment proof.');
-    }
-});
-flatpickr("#date", {
-    enableTime: false,
-    dateFormat: "Y-m-d",
-    minDate: "today", // Prevent booking past dates
-});
+function initClient() {
+    gapi.client.init({
+        apiKey: 'AIzaSyD2mx-zwA7E8oZO772GcELsB-XmoKGzH-A',
+        clientId: '885426849869-2bf14igjfbnihvq2vvcr3o5q0kp4anbg.apps.googleusercontent.com',
+        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+        scope: "https://www.googleapis.com/auth/calendar.readonly"
+    }).then(function () {
+        listUpcomingEvents();
+    }, function(error) {
+        console.log(JSON.stringify(error, null, 2));
+    });
+}
+
+function listUpcomingEvents() {
+    gapi.client.calendar.events.list({
+        'calendarId': 'primary', // Use your Google Calendar ID
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+    }).then(function(response) {
+        var events = response.result.items;
+        if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+                var event = events[i];
+                var when = event.start.dateTime || event.start.date;
+                console.log(event.summary + ' (' + when + ')');
+            }
+        } else {
+            console.log('No upcoming events found.');
+        }
+    });
+}
